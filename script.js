0a1fb24f8d4a2c30a37c986981b38d2483cd1436
Ümit <umitguzelyel17@gmail.com>
2025-03-22 22:28:51 +0300

    V1.3

--- script.js
+++ script.js
@@ -3,28 +3,34 @@ function contentAc()
 
     var panel = document.getElementById("escontent"); // Paneli seç
     var espresso = document.getElementById("espresso"); // Paneli seç
-        
-    if(panel.style.display == "none")
+    var americano = document.getElementById("americano"); // Paneli seç
+    var amecontent = document.getElementById("amecontent"); // Paneli seç
+
+ 
+    if (panel.style.display == "none")
     {
         panel.style.display = "block";
+        
+
+
+        espresso.style.borderBottomLeftRadius = "0px";
+        espresso.style.borderBottomRightRadius = "0px";
+        americano.style.top = "65%";
+        amecontent.style.top = "69%";
+        
     }
-    else
+
+    else  
     {
         panel.style.display = "none";
+        
         espresso.style.borderRadius = "20px";
+
+        americano.style.top = espresso.style.top + "21%";
+        amecontent.style.top = espresso.style.top + "25%";
     }
 
 
-
-
-
-
-
-
-
-
-    
-    
 }
 
 function togglebutton()
